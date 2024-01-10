fa7135921cd1b36e7693e5c735033156edf12e97
Álvaro Manterola <alvaro.manterola@gmail.com>
2024-01-09 23:04:59 -0300

    feat: added Preact and created the first island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://master--dreamy-cranachan-49927b.netlify.app/"
+  site: "https://master--dreamy-cranachan-49927b.netlify.app/",
+  integrations: [preact()]
 });
